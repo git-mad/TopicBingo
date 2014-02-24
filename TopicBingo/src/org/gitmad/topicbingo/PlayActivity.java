9f93f101f69a88a2890d2323e67483395198fca0
Eric Cochran <smartandmellow@gmail.com>
2014-02-23 23:11:50 -0500

    put the topics in a ListView so no hardcoding TextViews for topics

--- TopicBingo/src/org/gitmad/topicbingo/PlayActivity.java
+++ TopicBingo/src/org/gitmad/topicbingo/PlayActivity.java
@@ -3,13 +3,22 @@ package org.gitmad.topicbingo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class PlayActivity extends Activity {
+
+	private ListView mTopicsList;
+	private String[] mTopicsTitles;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_play);
+		mTopicsList = (ListView) findViewById(R.id.topics_list);
+		mTopicsTitles = getResources().getStringArray(R.array.topics_array);
+		mTopicsList.setAdapter(new ArrayAdapter<String>(this,
+      	        R.layout.topics_list_item, mTopicsTitles));
 	}
 
 	@Override
