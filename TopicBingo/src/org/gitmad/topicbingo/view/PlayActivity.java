ddef79f182859d538e9d7ce7fed4f2e89b420061
Taylor Peet <repeet13@gmail.com>
2014-03-23 11:04:32 -0400

    Added randomize button.

--- TopicBingo/src/org/gitmad/topicbingo/view/PlayActivity.java
+++ TopicBingo/src/org/gitmad/topicbingo/view/PlayActivity.java
@@ -17,31 +17,35 @@ import org.gitmad.topicbingo.model.Topic;
 public class PlayActivity extends Activity {
 
 	private ListView mTopicsList;
-    private DataModel model;
+	private DataModel model;
+	private TopicArrayAdapter mAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_play);
-        model = ((TopicBingoApplication)getApplication()).getModel();
+		model = ((TopicBingoApplication) getApplication()).getModel();
 
 		mTopicsList = (ListView) findViewById(R.id.topics_list);
-		mTopicsList.setAdapter(new TopicArrayAdapter(this,
-      	        model.getCurrentTopics(), new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(v.getClass()==TextView.class)
-                {
-                    TextView tv = (TextView)v;
-                    Topic t = model.getTopic(tv.getText()+"");
-                    t.setChecked(!t.isChecked()); // Flip whether checked or not
-                    tv.setTextColor(t.isChecked() ? Color.GREEN : Color.BLACK); // Toggle text color between Green and Black
-                    TextView count = (TextView) findViewById(R.id.count);
-                    count.setText(model.getCount());
-                }
-            }
-        }));
-    }
+		mAdapter = new TopicArrayAdapter(this, model.getCurrentTopics(),
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						if (v.getClass() == TextView.class) {
+							TextView tv = (TextView) v;
+							Topic t = model.getTopic(tv.getText() + "");
+							t.setChecked(!t.isChecked()); // Flip whether
+															// checked or not
+							tv.setTextColor(t.isChecked() ? Color.GREEN
+									: Color.BLACK); // Toggle text color between
+													// Green and Black
+							TextView count = (TextView) findViewById(R.id.count);
+							count.setText(model.getCount());
+						}
+					}
+				});
+		mTopicsList.setAdapter(mAdapter);
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,13 +53,20 @@ public class PlayActivity extends Activity {
 		getMenuInflater().inflate(R.menu.play, menu);
 		return true;
 	}
-	
+
 	public void notifyWin(View v) {
-		((TopicBingoApplication)getApplication()).sendNotification(TopicBingoApplication.notificationType.NOTIFY_WIN);
+		((TopicBingoApplication) getApplication())
+				.sendNotification(TopicBingoApplication.notificationType.NOTIFY_WIN);
 	}
-	
 
 	public void notifyLoss(View v) {
-		((TopicBingoApplication)getApplication()).sendNotification(TopicBingoApplication.notificationType.NOTIFY_LOSE);
+		((TopicBingoApplication) getApplication())
+				.sendNotification(TopicBingoApplication.notificationType.NOTIFY_LOSE);
+	}
+
+	public void RandomizeTopics(View v) {
+		// TODO randomize topics here
+		model.randomizeTopics();
+		mAdapter.notifyDataSetChanged();
 	}
 }
